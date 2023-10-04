313d01623fc1e69efc4fc2913ce6d64f0734e29a
DipeshC1707 <dipeshchavan8101@gmail.com>
2023-10-05 01:47:27 +0530

    push it

--- lib/views/category_screen/ItemDetails.dart
+++ lib/views/category_screen/ItemDetails.dart
@@ -7,7 +7,7 @@ import 'package:e_mart/consts/colors.dart';
 import 'package:velocity_x/velocity_x.dart';
 
 class ItemDetails extends StatelessWidget {
-  final String ? title;
+  final String? title;
   const ItemDetails({
     Key? key,
     required this.title,
@@ -15,27 +15,63 @@ class ItemDetails extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    
     return Scaffold(
       backgroundColor: lightGrey,
       appBar: AppBar(
-        title: title!.text.color(darkFontGrey).fontFamily(bold).make(),
-        actions: [
-        IconButton(onPressed: () {}, icon: const Icon(Icons.share)),
-        IconButton(onPressed: () {}, icon: const Icon(Icons.favorite_outline)),
-      ]),
+          title: title!.text.color(darkFontGrey).fontFamily(bold).make(),
+          actions: [
+            IconButton(onPressed: () {}, icon: const Icon(Icons.share)),
+            IconButton(
+                onPressed: () {}, icon: const Icon(Icons.favorite_outline)),
+          ]),
       body: Column(
         children: [
           Expanded(
             child: Container(
-              color: redColor,
+              padding: const EdgeInsets.all(8),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  VxSwiper.builder(
+                    autoPlay: true,
+                    height: 350,
+                    aspectRatio: 16/9,
+                    itemCount: 3,
+                    itemBuilder: (context, index) {
+                      return Image.asset(
+                        imgFc5,
+                        width: double.infinity,
+                        fit: BoxFit.cover,
+                      );
+                    },
+                  ),
+                  10.heightBox,
+                  title!.text.size(16).color(darkFontGrey).fontFamily(bold).make(),
+                  10.heightBox,
+                  VxRating(
+                    onRatingUpdate: (value){},
+                    normalColor: textfieldGrey,
+                    selectImage: golden,
+                    count: 5,
+                    size: 25,
+                    stepInt: true,
+                    ),
+                    10.heightBox,
+                    "30,000₹".text.color(redColor).fontFamily(bold).size(18).make()
+                ],
+              ),
             ),
           ),
           SizedBox(
-            child: button(color: redColor,onpress: (){},textcolor: whiteColor,title: "Add To Cart"),
+            width: double.infinity,
+            height: 60,
+            child: button(
+                color: redColor,
+                onpress: () {},
+                textcolor: whiteColor,
+                title: "Add To Cart"),
           )
         ],
-
       ),
     );
   }
